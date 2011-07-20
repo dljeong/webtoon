2fce961a44c00f48c95cf24105fcc42048931446
dljeong <jdl123123@naver.com>
2011-07-19 23:18:58 -0700

    Get the latest title

--- webtoon.rb
+++ webtoon.rb
@@ -7,4 +7,4 @@ page = agent.get("http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=
 
 #puts page.body
 
-puts page.search('//td[@class="title"]/a').map(&:inner_html)
+puts page.search('//td[@class="title"]/a')[0].inner_html#map(&:inner_html)
