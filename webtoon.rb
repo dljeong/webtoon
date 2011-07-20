426776bf254cb5047ea85a1d656184bf1865b406
dljeong <jdl123123@naver.com>
2011-07-19 23:34:30 -0700

    Program finished

--- webtoon.rb
+++ webtoon.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 require 'rubygems'
 require 'mechanize'
 
@@ -5,6 +7,12 @@ agent = Mechanize.new
 
 page = agent.get("http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=thu")
 
-title = page.search('//td[@class="title"]/a')[0].inner_html
+title = page.search('//td[@class="title"]/a')[2].inner_html
 
-puts title[title.index("(")+1...title.index(")")]
+begin
+	ind = title[title.index("(")+1...title.index(")")]
+	1/ind.to_i
+	puts "Time for sleep!"
+rescue
+	puts "Webtoon Time!"
+end
